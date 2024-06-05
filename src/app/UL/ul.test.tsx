2edce3522664d6ef70d5d4e981739ca5c2e3f5be
Dan Fyfe <fyfedaniel@gmail.com>
2024-06-05 16:48:51 -0400

    add test and handle ability to test throwing errors

--- src/app/UL/ul.test.tsx
+++ src/app/UL/ul.test.tsx
@@ -1,10 +1,10 @@
+// @ts-nocheck
 import { render } from "@testing-library/react";
 import UL from ".";
 
 describe('UL', () => {
-  
   it('does not error when children are LI elements', () => {
-    const component = render(
+    const component = () => render(
       <UL>
         <li></li>
         <li></li>
@@ -14,22 +14,30 @@ describe('UL', () => {
   });
 
   it('errors when children are not LI elements', () => {
-    const component = render(
+    const component = () => render(
       <UL>
         <p></p>
         <p></p>
       </UL>
     );
-    expect(component).toBeDefined();
+    expect(component).toThrow();
   });
 
   it('errors when one child is not an LI element', () => {
-    const component = render(
+    const component = () => render(
       <UL>
         <li></li>
         <p></p>
       </UL>
     );
-    expect(component).toBeDefined();
+    expect(component).toThrow();
+  });
+
+  it('errors when there are no children', () => {
+    const component = () => render(
+      <UL>
+      </UL>
+    );
+    expect(component).toThrow();
   });
 });
