37fb45e1b445145c8445b971d0710bd17cf97044
Dan Fyfe <fyfedaniel@gmail.com>
2024-06-05 15:23:26 -0400

    add jest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import UL from "./UL";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* testing area */}
       <UL>
         <li>hi</li>
         <li>hi 2</li>
