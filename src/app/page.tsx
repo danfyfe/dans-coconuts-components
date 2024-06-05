ce990712b834588a59eb5e661f76c79814f27157
Dan Fyfe <fyfedaniel@gmail.com>
2024-06-05 15:27:56 -0400

    clear main testing area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* testing area */}
-      <UL>
-        <li>hi</li>
-        <li>hi 2</li>
-      </UL>
     </main>
   );
 }
